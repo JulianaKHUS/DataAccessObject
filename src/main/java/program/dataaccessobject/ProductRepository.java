0322b9c6712ed1533f599d657f5dee68df444e79
otd6638 <iulianka99@mail.ru>
2024-12-24 17:44:08 +0300

    DataAccessObject

--- src/main/java/program/dataaccessobject/ProductRepository.java
+++ src/main/java/program/dataaccessobject/ProductRepository.java
@@ -1,3 +1,5 @@
+package program.dataaccessobject;
+
 import org.springframework.beans.factory.annotation.Autowired;import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
